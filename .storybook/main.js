8bac4df58559e79e15794ccef84ee9f0873a6813
Eiji Tachikawa <tsuyoku1202@gmail.com>
2022-12-09 17:58:14 +0900

    01 commit

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,7 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.js"],
   staticDirs: ["../public"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", "@storybook/addon-a11y"],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
